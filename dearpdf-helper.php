d6062bd00af773a78c62ddb18cfa9451aef2b17b
Pankaj Kumar <pankaj3359kr@gmail.com>
2021-10-04 16:55:18 +0530

    Added support for tags

--- dearpdf-helper.php
+++ dearpdf-helper.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Dearpdf Helper
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://pixelnib.com
+ * Description:     Helper plugin for DearPDF
+ * Author:          PixelNib
+ * Author URI:      https://pixelnib.com
  * Text Domain:     dearpdf-helper
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -13,10 +13,9 @@
  */
 
 
-add_action('wp_head', 'pn_header_script');
+//Adding tags to cpt
 
-function pn_header_script(){
-?>
-<h1>Hello World</h1>
-<?php
-};
+function reg_tag() {
+    register_taxonomy_for_object_type('post_tag', 'dearpdf');
+}
+add_action('init', 'reg_tag');
