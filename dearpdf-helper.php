610fda3f936d050ab3894c7b60edebea768d908b
Pankaj Kumar <pankaj3359kr@gmail.com>
2021-10-07 17:20:16 +0530

    Added Bootstrap

--- dearpdf-helper.php
+++ dearpdf-helper.php
@@ -82,3 +82,18 @@
 
     add_action( "after_dearpdf_single_content", "pn_after_single_content", 10, 1 );
 
+
+    
+    //Enque bootstrap
+	function hook_javascript() {
+        ?>
+            
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    
+            <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+    
+        <?php
+    }
+    add_action('wp_head', 'hook_javascript');
+    
