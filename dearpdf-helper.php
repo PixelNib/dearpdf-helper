18997d2f337bc6056019eb1aeb4cd902532dc192
Naveen Kharwar <naveenkharwar21@protonmail.com>
2021-10-09 23:32:47 +0530

    Improve cat. page

--- dearpdf-helper.php
+++ dearpdf-helper.php
@@ -1,4 +1,4 @@
-<?php
+d<?php
 /**
  * Plugin Name:     Dearpdf Helper
  * Plugin URI:      https://pixelnib.com
@@ -161,9 +161,55 @@ function gautam_brothers_system(){
 
 add_shortcode('gb_system', 'gautam_brothers_system');
 
+
+
+
+
+function gb_category_page_title() {
+    the_archive_title('<h1>', '</h1>');
+}
+
+add_action('before_dearpdf_category_content', 'gb_category_page_title');
+
+/**
+ * Function to open single page from the Category page.
+ */
+
+function pn_category_single_template_content(){
+
+    $current_term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+    $postslist = get_posts( array(
+        'post_type'      => 'dearpdf',
+        'posts_per_page' => -1,
+        'nopaging'       => true,
+        'tax_query'      => array( array(
+        'taxonomy' => 'dearpdf_category',
+        'field'    => 'slug',
+        'terms'    => $current_term->slug,
+    ) ),
+    ) );
+    echo '<div class="row">';
+
+    if ( count( $postslist ) > 0 ) {
+        foreach ( $postslist as $post ) {
+            $post_data = get_post_meta( $post->ID, '_dearpdf_data' ,true);
+            echo '<div class="col-md-3">';
+            echo '<a href="' . get_permalink( $post->ID ) . '">';
+            echo '<img src=" ' . $post_data['pdfThumb'] . ' " class="d-block img-fluid img-thumbnail"></img>';
+            echo '</a>';
+            echo  '</div>' ;
+        }
+    } else {
+        echo  '<h1>No Posts Found</h1><br>' ;
+    }
+    echo  '</div>' ;
+}
+
+add_action( 'dearpdf_category_content','pn_category_single_template_content' );
+
 //Equeue styles from assets
 function pn_add_to_head() {
-    if ( is_singular('dearpdf') ) {
+    if ( is_singular('dearpdf') || is_archive( 'dearpdf' ) ) {
             wp_enqueue_style( 'dearpdf-helper', plugins_url( '/assets/css/style.css', __FILE__ ) );
             wp_enqueue_script( 'dearpdf-helper', plugins_url( '/assets/js/script.js', __FILE__ ) );
     }
